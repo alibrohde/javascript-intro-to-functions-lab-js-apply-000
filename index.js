18a6b5a1abc3299d8f53e92032407fa75f62f774
Ali <alibrohde@gmail.com>
2018-12-09 23:39:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 
+<<<<<<< HEAD
 
 
 function sayHiToGrandma(string) {
@@ -28,3 +29,31 @@ function sayHiToGrandma(string) {
   return "I love you, too." 
 }
 }
+=======
+var lowercase = 'hello!'
+var uppercase = "HELLO!"
+
+function sayHiToGrandma(string) {
+  if (string === lowercase) {
+    return "I can't hear you!"
+  }
+  if (string === uppercase) {
+    return "YES INDEED"
+  }
+  if (string === "I ")
+  return string 
+}
+describe('sayHiToGrandma(string)', function() {
+  it('returns "I can\'t hear you!" if `string` is lowercase', function() {
+    expect(sayHiToGrandma('hello')).toEqual("I can't hear you!")
+  })
+
+  it('returns "YES INDEED!" if `string` is uppercase', function() {
+    expect(sayHiToGrandma('HELLO')).toEqual("YES INDEED!")
+  })
+
+  it('returns "I love you, too." if `string` is "I love you, Grandma."`', function() {
+    expect(sayHiToGrandma("I love you, Grandma.")).toEqual("I love you, too.")
+  })
+})
+>>>>>>> bab671fe269f238c887335c2c92c487c9e420e8b
